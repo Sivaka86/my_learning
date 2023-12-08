5c36d78e4fc15bf54f81a662c1cb4c6d506f1fe8
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-12-08 10:00:20 -0800

    Create LearnJavaForSelenium.java
    print

---
+++ LearnJavaForSelenium.java
@@ -0,0 +1,11 @@
+public class LearnJavaForSelenium {
+	public static void main(String args[]) {
+		
+		  System.out.println("Welcome to Learn Java for Automation");
+		  System.out.println("Selenium is web based Automation Tool");
+		  System.out.println(100); System.out.println("100");
+		  System.out.println(20+30);
+		 
+	}
+
+}
