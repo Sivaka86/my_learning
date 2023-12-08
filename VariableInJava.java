cf7b181c6769bde0dd80031ab782239d5bbb5583
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-12-08 10:03:01 -0800

    Create VariableInJava.java
    Variable declaration

---
+++ VariableInJava.java
@@ -0,0 +1,37 @@
+public class VariableInJava {
+
+	public static void main(String[] args) {
+		
+		  int x =5; //variable declaration 
+		  int y; //variable initialization 
+		  y=8; // Assigning the value to the variable 
+		  //method 1 
+		  int g=3; 
+		  int h=4;
+		  int i=5;
+		  
+		  // method 2 
+		  int e,t,k;
+		  e=3;
+		  t=4;
+		  k=5;
+		 
+
+		//method 3 when data type is same for all 
+		int a=3,b=4,c=5;
+
+		//retrieve the value
+		 System.out.println(a); // 3
+		 System.out.println(b); //4
+		 System.out.println(c); //5
+
+		 System.out.println(a+b+c); //12
+
+		 System.out.println(a+" "+b+" "+c); //3 4 5
+
+		 System.out.println("Value of a is :"+a); //Value of a is :3
+		 System.out.println("Value of b is :"+b); //Value of b is :4
+		 System.out.println("Value of c is :"+c); //Value of c is :5
+	}
+
+}
